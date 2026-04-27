114beff8fa7c743c604771526da73f20c84143b4
devchampl00 <alfariziuchiha@gmail.com>
2026-04-27 13:42:39 +0800

    nyoba

--- app/src/main/java/com/example/tes/MainActivity.kt
+++ app/src/main/java/com/example/tes/MainActivity.kt
@@ -10,12 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val textView = findViewById<TextView>(R.id.textView)
-        val button = findViewById<Button>(R.id.button)
-
-        button.setOnClickListener {
-            textView.text = "Halo dari Kotlin!"
-        }
     }
 }
